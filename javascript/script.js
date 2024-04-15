675df9ad2359955aa943a5d0b1a4ed48ce76e8ae
GauravVashist239247 <gvashist1010@gmail.com>
2024-04-15 12:16:56 +0530

    fir

---
+++ javascript/script.js
@@ -0,0 +1,206 @@
+// function getData(){
+//     {
+//         var a=12;
+//         let b=4;
+//         const c=6;
+
+//     }
+//     console.log(a)     
+//     console.log(b)
+//     console.log(c)   
+// }
+// getData()
+
+
+
+
+// let object = {
+//     Name: "John",
+//     Age: 21
+// };
+// console.log(object)
+
+
+
+
+
+// let a=10;
+// let b=2;
+// console.log("a =" +a);
+// console.log("b =" +b);
+// let sum= a+b;
+// let diff=a-b;
+// let product =a*b;
+// let quo=a/b;
+// let re=a%b;
+
+// console.log("a+b=", sum);
+// console.log("a-b=", diff);
+// console.log("a*b=", product);
+// console.log("a/b=", quo);
+// console.log("a%b=", re);
+
+
+
+
+
+
+// console.log(10==5)
+// console.log(10<=5)
+// console.log(10>=5)
+// console.log(10<=10)
+// console.log(10<10)
+
+
+
+
+// a.push("gaurav")
+// console.log(a)
+
+
+
+
+
+
+
+
+
+// let a=[1,2,3];
+// console.log(a);
+// a.shift();
+// console.log(a);
+// a.unshift(1);
+
+// console.log(a);
+// a.length
+
+
+
+
+// let i=0;
+// while(i<10)
+// {
+//     i=i+1;
+//     console.log("hello" + i);
+// }
+
+
+
+
+
+
+
+
+// let i=0;
+// const ar=[1,2,3,4,5,6,7];
+// while(i<ar.length){
+   
+//     console.log(ar[i]);
+//      i=i+1;
+// }
+
+
+
+
+
+
+
+// let array=[1,2,3,4];
+// for(let i=0;i<array.length; ++i)
+
+// {
+//     console.log(array[i]);
+// }
+
+
+
+
+// const list=['a','b','c','d','e','f'];
+// for(const value of list){
+//     console.log(value);
+// }
+
+
+
+
+
+// function add(a,b)
+// {
+//     let sum=a+b;
+//     console.log(sum)
+// }
+
+
+
+
+
+
+
+function sub(a,b){
+    let diff=a-b;
+    return diff
+}
+let diff=sub(120,10);
+console.log(diff);
+
+
+
+
+
+
+
+
+function add(a,b){
+    let sum=a+b;
+    return sum
+}
+let sum=add(120,10);
+console.log(sum);
+
+
+
+
+
+
+
+
+
+
+
+function prod(a,b){
+    let mul=a*b;
+    return mul
+}
+let mul=prod(120,10);
+console.log(mul);
+
+
+
+
+
+
+
+
+
+function quo(a,b){
+    let divv=a/b;
+    return divv
+}
+let divv=quo(120,10);
+console.log(divv);
+
+
+
+
+
+
+
+
+
+
+function rem(a,b){
+    let mod=a%b;
+    return mod
+}
+let mod=rem(120,10);
+console.log(divv);
